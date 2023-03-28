27c4e135714dd566ad548d8a0dbef0e2113341d5
@hexlet/cli <support@hexlet.io>
2023-03-28 18:01:03 +0200

    submit ruby-basics-ru/testing

---
+++ ruby-basics-ru/testing/lib/stack.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Stack
+  def initialize(elements = [])
+    @elements = elements
+  end
+
+  def pop!
+    Stack.new
+    @elements.pop
+  end
+
+  def push!(element)
+    @elements << element
+  end
+
+  def empty?
+    @elements.empty?
+  end
+
+  def to_a
+    @elements
+  end
+
+  def clear!
+    @elements = []
+  end
+
+  def size
+    @elements.size
+  end
+end
---
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+require_relative '../lib/stack'
+
+class StackTest < Minitest::Test
+  # BEGIN
+  def test_push!
+    stack = Stack.new %w[foo bar]
+    stack.push! 'some'
+    stack.push! 'element'
+
+    assert { stack.to_a == %w[foo bar some element] }
+    assert { stack.size == 5 }
+  end
+
+  def test_pop
+    stack = Stack.new %w[one two three]
+    stack.pop!
+    stack.pop!
+
+    assert { stack.to_a == %w[one] }
+    assert { stack.size == 1 }
+  end
+
+  def test_clear!
+    stack = Stack.new %w[one two three]
+    assert { !stack.empty? }
+
+    stack.clear!
+    assert { stack.empty? }
+  end
+
+  def test_empty_stack
+    stack = Stack.new
+
+    assert { stack.empty? == true }
+    assert { stack.size.zero? }
+    assert { stack.to_a == [] }
+    assert { stack.pop!.nil? }
+  end
+  # END
+end
+
+test_methods = StackTest.new({}).methods.select { |method| method.start_with? 'test_' }
+raise 'StackTest has not tests!' if test_methods.empty?
